312a9b96049cac1bbd5ebd255bccce1e4b88d24f
jorge6217 <jorge6217@gmail.com>
2023-01-28 15:13:20 -0600

    IStack class methods

--- src/hojaDos/IStack.java
+++ src/hojaDos/IStack.java
@@ -1,5 +1,23 @@
 package hojaDos;
+import java.util.EmptyStackException;
 
-public class IStack {
+public class IStack<T> {
+	
+	   public void push(T item) {
+	}
+	   public T pop() throws EmptyStackException {
+		return null;
+	}
+	   public T peek() throws EmptyStackException {
+		return null;
+	}
+	   public boolean empty() {
+		return false;
+	}
+	   public int size() {
+		return 0;
+	}
 
 }
+
+
