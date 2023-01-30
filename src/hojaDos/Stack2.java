a9debd0c8db7d1967dea6a83306028d033fd6d0c
jorge6217 <jorge6217@gmail.com>
2023-01-30 10:07:48 -0600

    Stack2 methods

--- src/hojaDos/Stack2.java
+++ src/hojaDos/Stack2.java
@@ -1,5 +1,53 @@
 package hojaDos;
+import java.util.EmptyStackException;
+import java.util.ArrayList;
 
-public class Stack2 implements IStack<float> {
+public class Stack2 implements IStack<Double> {
+	ArrayList<Double> stack = null;
+
+    public void clasestack(){
+        stack = new ArrayList<Double>();
+    }
+
+    public void push(Double item){
+        stack.add(item);
+    }
+
+
+   public Double pop() throws EmptyStackException {
+        if (empty()){
+            return null;
+        }
+        else{
+            Double op =  stack.get(stack.size()-1);
+            stack.remove(stack.size()-1);
+            return op;
+        }
+   }
+
+
+   public Double peek() throws EmptyStackException{
+        if (empty()){
+            return null;
+        }
+        else{
+            return stack.get(stack.size()-1);
+        }
+   }
+
+
+   public boolean empty(){
+        if (stack.size()==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+   }
+
+
+   public int size(){
+        return stack.size();
+   }
 
 }
