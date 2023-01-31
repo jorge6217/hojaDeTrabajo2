ff2131f837b3923abf066c4e64333dd4e6e6250c
jorge6217 <jorge6217@gmail.com>
2023-01-30 18:23:55 -0600

    text reader added on main class

--- src/hojaDos/Main.java
+++ src/hojaDos/Main.java
@@ -1,5 +1,44 @@
 package hojaDos;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+	static calculatorClass c = new calculatorClass();
+    public static void main(String[] args) {
+        leerArchivo();
+    }
+    private static void leerArchivo(){
+        String nombreFichero = "datos.txt";
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(nombreFichero));
+            String texto = br.readLine();
+            while(texto != null) {
+                texto = br.readLine();
+                String se = c.remover(texto);
+                Double res = c.evaluate(se);
+                System.out.println("El resultado es  "+ res);
+            }
+        }
+        catch (FileNotFoundException ex) {
+            System.out.println("Error: Archivo de texto no encontrado");
+            ex.printStackTrace();
+        }
+        catch(Exception ex) {
+            System.out.println(" ");
+            ex.printStackTrace();
+        }
+        finally {
+            try {
+                if(br != null) {
+                    br.close();
+                }
+            }
+            catch (Exception ex) {
+                System.out.println("Error al cerrar el archivo de texto");
+                ex.printStackTrace();
+            }
+        }
+    }
 
 }
