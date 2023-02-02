030b219d7a25c9781fe5c238c81570cff7cd94f2
jorge6217 <jorge6217@gmail.com>
2023-02-01 20:11:33 -0600

    ICalculator class comments

--- src/hojaDos/ICalculator.java
+++ src/hojaDos/ICalculator.java
@@ -1,8 +1,36 @@
+/**
+ * ICalculator
+ * 
+ * Interfaz que representa una calculadora en el sistema.
+ */
 package hojaDos;
-//Calcula la expresión postfix dada
+
+/**
+ * ICalculator
+ * 
+ * Interfaz que representa una calculadora en el sistema.
+ */
 public interface ICalculator {
-	//Obtiene el resultado de la operación anterior.
-	public double evaluate(String expression) throws ArithmeticException;
-	public double getPrevious();
-	
+    /**
+     * Calcula la expresión postfix dada.
+     * La expresión postfix debe ser una expresión válida, lo que significa
+     * que toda validación de formato a realizar debe hacerse antes de llamar a este
+     * método.
+     * 
+     * La excepción se encuentra en caso se encuentre alguna división entre 0
+     * o algún otro caso de excepción aritmética.
+     * 
+     * @param expression La expresion a calcular.
+     * @return El resultado de la expresión.
+     */
+    public double evaluate(String expression) throws ArithmeticException;
+
+    /**
+     * Obtiene el resultado de la operación anterior.
+     * 
+     * En caso no exista una operación anterior, retorna 0.
+     * 
+     * @return El resultado de la operación anterior.
+     */
+    public double getPrevious();
 }
