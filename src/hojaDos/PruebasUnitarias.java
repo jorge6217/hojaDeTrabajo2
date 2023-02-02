434e01965da2599fc0b80ba22358ec5c9c442ac4
jorge6217 <jorge6217@gmail.com>
2023-02-01 20:34:20 -0600

    created PruebasUnitarias class

---
+++ src/hojaDos/PruebasUnitarias.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package hojaDos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author jorge lopez 221038
+ *
+ */
+class PruebasUnitarias {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
